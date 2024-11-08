a0490cfaa08c73d14e5c55b4a041b0841466232e
Michal Lata <michallata.dev@gmail.com>
2024-11-08 22:38:55 +0100

    feat: Add Header component (without styles)

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { CURRENT_CATEGORY } from '@/constants';
+
+export const Header = () => {
+	return (
+		<header>
+			<h1>Samsung – {CURRENT_CATEGORY}</h1>
+			<h2>Wybierz urządzenie</h2>
+		</header>
+	);
+};
