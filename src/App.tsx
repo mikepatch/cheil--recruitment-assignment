31930f8f89a27465929759141787c797002d5181
Michal Lata <michallata.dev@gmail.com>
2024-11-10 00:04:10 +0100

    patch: Update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
+import { Header } from '@/components/Header';
+import { ProductListing } from '@/components/product-listing/ProductListing';
+import { FilterSection } from '@/components/filter-section/FilterSection';
+import { ProductsProvider } from '@/context/ProductsContext';
 
 function App() {
 	return (
-		<>
-			<h1>Hello world</h1>
-		</>
+		<ProductsProvider>
+			<Header />
+			<main>
+				<FilterSection />
+				<ProductListing />
+			</main>
+		</ProductsProvider>
 	);
 }
 
