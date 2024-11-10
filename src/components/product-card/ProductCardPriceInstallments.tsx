5917c7d68d130f43411450bf1159176a532a04c4
Michal Lata <michallata.dev@gmail.com>
2024-11-10 19:00:50 +0100

    feat: Add ProductCardInstallments component

---
+++ src/components/product-card/ProductCardPriceInstallments.tsx
@@ -0,0 +1,21 @@
+import { type InstallmentPlan } from '@/types';
+
+type ProductCardPriceInstallmentsProps = {
+	installmentPlan: InstallmentPlan;
+};
+
+export const ProductCardPriceInstallments = ({
+	installmentPlan,
+}: ProductCardPriceInstallmentsProps) => {
+	const formattedMonthlyAmount = new Intl.NumberFormat('pl-PL', {
+		style: 'currency',
+		currency: installmentPlan.currency,
+		minimumFractionDigits: 2,
+	}).format(installmentPlan.monthlyAmount);
+
+	return (
+		<p aria-label="Plan ratalny" className="py-4 text-base font-bold text-gray-600">
+			<span>{formattedMonthlyAmount}</span> x <span>{installmentPlan.months}</span> rat
+		</p>
+	);
+};
