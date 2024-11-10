59acd601cb86c821a7897714e927ab846f800685
Michal Lata <michallata.dev@gmail.com>
2024-11-10 18:50:10 +0100

    feat: Move and refactor the ProductCard component

---
+++ src/components/product-card/ProductCard.tsx
@@ -0,0 +1,32 @@
+import { ProductCardButton } from '@/components/product-card/ProductCardButton';
+import { ProductCardEnergyLabel } from '@/components/product-card/ProductCardEnergyLabel';
+import { ProductCardHeader } from '@/components/product-card/ProductCardHeader';
+import { ProductCardPriceSection } from '@/components/product-card/ProductCardPriceSection';
+import { ProductCardSpecs } from '@/components/product-card/ProductCardSpecs';
+import { useProducts } from '@/hooks/useProducts';
+import { type Product } from '@/types';
+
+type ProductCardProps = {
+	product: Product;
+};
+
+export const ProductCard = ({ product }: ProductCardProps) => {
+	const { selectedProductIds, setSelectedProductIds } = useProducts();
+	const isProductSelected = selectedProductIds.includes(product.id);
+
+	const handleSelectProduct = () => {
+		setSelectedProductIds(product.id);
+	};
+
+	return (
+		<div className="rounded-2xl bg-white px-6 py-4 flex flex-col">
+			<ProductCardHeader productId={product.id} productName={product.name} />
+			<section className="text-xs">
+				<ProductCardSpecs productData={product} />
+				<ProductCardEnergyLabel energyClass={product.energyClass} />
+				<ProductCardPriceSection price={product.price} installmentPlan={product.installmentPlan} />
+			</section>
+			<ProductCardButton isProductSelected={isProductSelected} onChange={handleSelectProduct} />
+		</div>
+	);
+};
