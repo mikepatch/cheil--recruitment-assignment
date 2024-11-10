af77844a5f1b8e88cf86225223e351291835f053
Michal Lata <michallata.dev@gmail.com>
2024-11-10 19:00:38 +0100

    feat: Add ProductCardPrice component

---
+++ src/components/product-card/ProductCardPrice.tsx
@@ -0,0 +1,29 @@
+import { type ProductPrice } from '@/types';
+
+type PriceProps = {
+	price: ProductPrice;
+};
+
+export const ProductCardPrice = ({ price }: PriceProps) => {
+	const formattedAmount = new Intl.NumberFormat('pl-PL', {
+		style: 'currency',
+		currency: price.currency,
+		minimumFractionDigits: 2,
+	}).format(price.amount);
+
+	const [amount, decimals, currency] = formattedAmount.split(/[\s,]/);
+	const [firstDigit, ...restDigits] = amount.split('');
+
+	return (
+		<p className="mt-1 flex items-start font-bold leading-none">
+			<span className="sr-only">Cena:</span>
+			<span className="text-[40px] font-bold tracking-tighter">
+				{firstDigit} {restDigits}
+			</span>
+			<div className="ml-2 flex flex-col text-right">
+				<span className="translate-y-1 transform text-sm">{decimals}</span>
+				<span className="text-sm">{currency}</span>
+			</div>
+		</p>
+	);
+};
