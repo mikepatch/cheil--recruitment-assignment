27352d6190b77433ddffcf23f1ea5453421ab8ff
Michal Lata <michallata.dev@gmail.com>
2024-11-10 22:34:54 +0100

    feat: Implement debouncing in SearchBar

--- src/components/filter-section/SearchBar.tsx
+++ src/components/filter-section/SearchBar.tsx
@@ -1,8 +1,18 @@
+import { useDebounce } from '@/hooks/useDebounce';
+import { useEffect, useState } from 'react';
+
 type SearchBarProps = {
 	onChange: (value: string) => void;
 };
 
 export const SearchBar = ({ onChange }: SearchBarProps) => {
+	const [searchTerm, setSearchTerm] = useState('');
+	const debouncedSearchTerm = useDebounce(searchTerm);
+
+	useEffect(() => {
+		onChange(debouncedSearchTerm);
+	}, [debouncedSearchTerm, onChange]);
+
 	return (
 		<div className="pb-2 pt-6 text-center" role="search">
 			<label className="sr-only" htmlFor="product-search">
@@ -13,7 +23,7 @@ export const SearchBar = ({ onChange }: SearchBarProps) => {
 				id="product-search"
 				placeholder="Szukaj..."
 				className="w-64 rounded-sm p-2 px-3 text-sm placeholder-black shadow-sm"
-				onChange={(e) => onChange(e.target.value)}
+				onChange={(e) => setSearchTerm(e.target.value)}
 			/>
 		</div>
 	);
