71b36ff375b453ba407d7135ffb1e507f4dcf879
Michal Lata <michallata.dev@gmail.com>
2024-11-10 05:04:27 +0100

    feat: Move Header and add styles

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import { CURRENT_CATEGORY } from '@/constants';
+
+export const Header = () => {
+	return (
+		<header className="py-6 text-center">
+			<h1 className="sr-only">Samsung – {CURRENT_CATEGORY}</h1>
+			<h2 className="text-3xl font-bold md:text-[40px]">Wybierz urządzenie</h2>
+		</header>
+	);
+};
