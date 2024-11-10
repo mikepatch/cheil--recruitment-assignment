7eadea1e3df3e1a91355bed0ec93fc45c257169c
Michal Lata <michallata.dev@gmail.com>
2024-11-10 18:56:02 +0100

    feat: Add ProductCardSpecs component

---
+++ src/components/product-card/ProductCardSpecs.tsx
@@ -0,0 +1,37 @@
+import { ProductCardSpecsItem } from '@/components/product-card/ProductCardSpecsItem';
+import { CATEGORY_CONFIG } from '@/constants';
+import { type Product } from '@/types';
+
+type ProductCardSpecsProps = {
+	productData: Product;
+};
+
+export const ProductCardSpecs = ({ productData }: ProductCardSpecsProps) => {
+	const { category, specifications, features } = productData;
+
+	const getAttributeLabel = (index: number) =>
+		CATEGORY_CONFIG[category]?.specificationAttributes[index].label;
+	const getFeaturesValue = features?.map(({ name }) => name).join(', ');
+
+	const renderSpecs = () => {
+		return Object.keys(specifications).map((attribute, index) => (
+			<ProductCardSpecsItem
+				key={`attribute-${index}`}
+				attributeKey={`${getAttributeLabel(index)}: `}
+				attributeValue={specifications[attribute]}
+			/>
+		));
+	};
+	const renderFeatures = () => {
+		if (!getFeaturesValue || getFeaturesValue.length <= 0) return '';
+
+		return <ProductCardSpecsItem attributeKey="Funkcje: " attributeValue={getFeaturesValue} />;
+	};
+
+	return (
+		<ul className="flex flex-col gap-px" aria-label="Specyfikacja produktu">
+			{renderSpecs()}
+			{renderFeatures()}
+		</ul>
+	);
+};
